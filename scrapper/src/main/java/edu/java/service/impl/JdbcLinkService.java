e3725d3486c3c363d01c7c031bd4e8a05e6f3bac
Kirill <dekerilljr@gmail.com>
2024-03-23 18:37:02 +0400

    feat: deleted update link in save methods

--- scrapper/src/main/java/edu/java/service/impl/JdbcLinkService.java
+++ scrapper/src/main/java/edu/java/service/impl/JdbcLinkService.java
@@ -6,7 +6,6 @@ import edu.java.model.Link;
 import edu.java.repository.jdbc.JdbcLinkRepository;
 import edu.java.repository.jdbc.JdbcSiteRepository;
 import edu.java.service.LinkService;
-import edu.java.util.updateChecker.UpdateChecker;
 import java.net.URI;
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -18,21 +17,10 @@ import org.springframework.stereotype.Service;
 public class JdbcLinkService implements LinkService {
     private final JdbcLinkRepository repository;
     private final JdbcSiteRepository jdbcSiteRepository;
-    private final List<UpdateChecker> updateCheckerList;
 
     @Override
     public LinkResponse addUserLink(Long chatId, URI url) {
         Link link = new Link(null, url, OffsetDateTime.now(), parseSite(url));
-        for (UpdateChecker checker : updateCheckerList) {
-            if (checker.isAppropriateLink(link)) {
-                try {
-                    checker.checkUpdates(link);
-                } catch (RuntimeException e) {
-                    throw new RuntimeException("Incorrect link, please try again");
-                }
-            }
-        }
-
         repository.saveUserLink(chatId, link);
         return new LinkResponse(repository.findByUrl(url).get().getId(), url);
     }
